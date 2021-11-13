13c3ec5727ccd0590164b52b23fa0224e2106dfa
AnisAmeloune <lucas.boulardb@icloud.com>
2021-11-13 15:56:53 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,104 @@
+// fun1
+// function equal(a,b,c) {
+//     if ( a==b && b==c) {
+//         return true;
+//     } else{
+//         return false
+//     }
+// }
+// console.log(equal("test","test","test"))
+// console.log(equal("test","test","fast"))
+
+
+
+// fun2
+// function convert(minutes){
+//     return minutes*60}
+//     console.log(convert("5"))
+//     console.log(convert("2"))
+
+
+
+// fun3
+//     function area(height,width){
+// return height*width}
+// console.log(area("2","3"))
+
+
+
+// fun4
+// function nextinteger(integer){
+//     return integer+1
+// }
+// console.log(nextinteger(7))
+// console.log(nextinteger(10))
+
+
+
+// fun5
+// function pointcounter(win,draw,defeat){
+// return win*3+draw+defeat*0}
+// console.log(pointcounter(10,4,16))
+// console.log(pointcounter(10,10,10))
+
+
+
+// fun6
+// function between(integer){
+// if( 100<integer && integer<200 ){
+//     return true
+// } else{
+//     return false
+// }}
+// console.log(between(300))
+// console.log(between(126))
+
+
+
+// fun7
+// function converttosec(hour,min){
+//   return  hour*3600+min*60
+// }
+// console.log(converttosec(1,1))
+// console.log(converttosec(92,35))
+
+
+
+// fun8
+// function equalitycheck(a,b){
+// if(a===b){ return true} else{return false}
+// }
+// console.log(equalitycheck(4,"4"))
+// console.log(equalitycheck("9","9"))
+
+// var jours = ["samedi", "dimanche", "lundi"];
+// for (let i = 0; i < jours.length; i++) {
+//   console.log(jours[i]);
+// }
+
+
+
+
+// fun9
+// function comparestrings(a,b){
+
+//     if(a.lenght > b.lenght){
+//         return "the first string is taller"
+//     }
+//     else { return " the second string is taller"}
+// }
+// console.log(comparestrings("amir","amiiir"))
+
+
+
+// fun10
+// function fullname(a,b){
+//     return a+" "+b
+// }
+// console.log(fullname("Amir","Dellachi"))
+// console.log(fullname('mikora',"omar"))
+
+
+ 
+
+
